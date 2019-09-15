f4d0f0c8515c43d9d5d3bf63ccc89e3b2c18e950
superdev358 <joshuaskitchen91+2@gmail.com>
2019-09-15 15:07:35 +0100

    hooked up survey results routes based on votes, users can now see survey outcomes

---
+++ client/src/components/surveys/formFields.js
@@ -0,0 +1,6 @@
+export default [
+    {label: "survey title", name: "title"}, 
+    {label: "subject line", name: "subject"},
+    {label: "email body", name: "body"},
+    {label: "recipient list", name: "recipients"},
+]
