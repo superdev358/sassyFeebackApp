b79848300c26f63214f0881259ef17f0e45d0791
Rudi Hinds <rudihinds@gmail.com>
2019-09-15 15:07:35 +0100

    hooked up survey results routes based on votes, users can now see survey outcomes

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -4,13 +4,9 @@ import React, { Component } from 'react';
 import { reduxForm, Field } from 'redux-form'
 import SurveyField from './SurveyField'
 import { Link } from 'react-router-dom'
+import validateEmails from '../../utils/validateEmails'
+import formFields from './formFields'
 
- const FIELDS = [
-        {label: "survey title", name: "title"}, 
-        {label: "subject line", name: "subject"},
-        {label: "email body", name: "body"},
-        {label: "recipient list", name: "emails"},
-    ]
 
 class SurveyForm extends Component {
 
@@ -18,16 +14,15 @@ class SurveyForm extends Component {
 
     renderFields = () => {
         
-        return FIELDS.map(field => <Field label={field.label} type="text" name={field.name} component={SurveyField}/>)
+        return formFields.map(({ name, label }) => <Field key={name} label={label} type="text" name={name} component={SurveyField}/>)
     }
        
     render() {
         return (
             <div>
                 Survey Form Component
-                <form onSubmit={this.props.handleSubmit(values => console.log(values) )}>
+                <form onSubmit={this.props.handleSubmit(this.props.onSurveySubmit)}>
                 {this.renderFields()}
-                
                 <Link to="/surveys" className="red btn-flat left white-text">Cancel</Link>
                 <button className="teal btn-flat right white-text" type="submit">
                     Next <i className="material-icons right">done</i>
@@ -40,38 +35,21 @@ class SurveyForm extends Component {
 
 const validate = values => {
     const errors = {}
-    
-    FIELDS.forEach(function({ name }){
+     
+    errors.recipients = validateEmails(values.recipients || '')
+    formFields.forEach(function({ name }){
         if (!values[name]){
             errors[name] = "this field must be filled in"
         }
   
     })
+
     return errors
 }
-//     return errors
-// }
-//     console.log(values)
-//     const errors = {}
-//     if (!values.title) {
-//         errors.title = "you must provide a title"
-//     }
-//     if (!values.subject) {
-//         errors.title = "you must provide a title"
-//     }
-//     if (!values.title) {
-//         errors.title = "you must provide a title"
-//     }
-//     if (!values.title) {
-//         errors.title = "you must provide a title"
-//     }
-
-
-//     return errors
-// }
 
 export default reduxForm({
     validate,
-    form: "surveyForm"
+    form: "surveyForm",
+    destroyOnUnmount: false
 })(SurveyForm);
 
